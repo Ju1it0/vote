dd47b62e4d1d4673a36b910dcfb36c22522aad14
Ju1it0 <julio.belbey@gmail.com>
2025-03-25 15:01:13 -0300

    feat: instalacion de React 19

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div className="container">
+            <h1>Bienvenido a Laravel + React</h1>
+        </div>
+    );
+} 
