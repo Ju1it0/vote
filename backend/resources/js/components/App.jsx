e7dc35255878e4c70db351c37f2898e5747468ad
Ju1it0 <julio.belbey@gmail.com>
2025-03-27 10:09:49 -0300

    refactor: eliminar implementación de React y Vite

--- backend/resources/js/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-    return (
-        <div className="container">
-            <h1>Bienvenido a Laravel + React</h1>
-        </div>
-    );
-} 
